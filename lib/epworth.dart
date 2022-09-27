2830c34d38d01386b7307005df94beb724911c18
lyahoff <lyahoff.vlad@gmail.com>
2022-09-27 14:36:02 +0600

    epworth is done

--- lib/epworth.dart
+++ lib/epworth.dart
@@ -52,6 +52,14 @@ class EpworthLogic extends StatefulWidget {
 }
 
 class _EpworthLogicState extends State<EpworthLogic> {
+  int read = 0;
+  int tv = 0;
+  int theatre = 0;
+  int car = 0;
+  int bed = 0;
+  int alcohol = 0;
+  int speak = 0;
+  int drive = 0;
   @override
   Widget build(BuildContext context) {
     return ListView(
@@ -63,8 +71,340 @@ class _EpworthLogicState extends State<EpworthLogic> {
             child: ListTile(
               title: h1Text(
                   '''Шкала сонливости Эпворта - это анкетный опрос, составленный специально для оценки сонливости и вероятности наличия нарколепсии. Все, что от Вас требуется - ответить на несколько вопросов касательно вероятности заснуть в описанных ситуациях. По завершению тестирования Вам будет выдан результат с расшифровкой и рекомендациями.'''),
-            ))
+            )),
+        Card(
+          elevation: 2,
+          child: Column(
+            children: [
+              ListTile(
+                title: h1Text('Вероятность задремать читая книгу сидя'),
+              ),
+              RadioListTile(
+                  title: h2Text('Никогда бы не задремал'),
+                  value: read,
+                  groupValue: 0,
+                  onChanged: ((value) => setState(() {
+                        read = 0;
+                      }))),
+              RadioListTile(
+                  title: h2Text('Есть небольшой шанс задремать'),
+                  value: read,
+                  groupValue: 1,
+                  onChanged: ((value) => setState(() {
+                        read = 1;
+                      }))),
+              RadioListTile(
+                  title: h2Text('Есть средняя вероятность задремать'),
+                  value: read,
+                  groupValue: 2,
+                  onChanged: ((value) => setState(() {
+                        read = 2;
+                      }))),
+              RadioListTile(
+                  title: h2Text('Есть высокая вероятность задремать'),
+                  value: read,
+                  groupValue: 3,
+                  onChanged: ((value) => setState(() {
+                        read = 3;
+                      }))),
+            ],
+          ),
+        ),
+        Card(
+            elevation: 2,
+            child: Column(
+              children: [
+                ListTile(
+                  title:
+                      h1Text('Вероятность задремать просматривая телепередачи'),
+                ),
+                RadioListTile(
+                    title: h2Text('Никогда бы не задремал'),
+                    value: tv,
+                    groupValue: 0,
+                    onChanged: ((value) => setState(() {
+                          tv = 0;
+                        }))),
+                RadioListTile(
+                    title: h2Text('Есть небольшой шанс задремать'),
+                    value: tv,
+                    groupValue: 1,
+                    onChanged: ((value) => setState(() {
+                          tv = 1;
+                        }))),
+                RadioListTile(
+                    title: h2Text('Есть средняя вероятность задремать'),
+                    value: tv,
+                    groupValue: 2,
+                    onChanged: ((value) => setState(() {
+                          tv = 2;
+                        }))),
+                RadioListTile(
+                    title: h2Text('Есть высокая вероятность задремать'),
+                    value: tv,
+                    groupValue: 3,
+                    onChanged: ((value) => setState(() {
+                          tv = 3;
+                        }))),
+              ],
+            )),
+        Card(
+            elevation: 2,
+            child: Column(
+              children: [
+                ListTile(
+                  title: h1Text(
+                      'Вероятность задремать сидя в общественном месте без проявления активности (в театре или на собрании)'),
+                ),
+                RadioListTile(
+                    title: h2Text('Никогда бы не задремал'),
+                    value: theatre,
+                    groupValue: 0,
+                    onChanged: ((value) => setState(() {
+                          theatre = 0;
+                        }))),
+                RadioListTile(
+                    title: h2Text('Есть небольшой шанс задремать'),
+                    value: theatre,
+                    groupValue: 1,
+                    onChanged: ((value) => setState(() {
+                          theatre = 1;
+                        }))),
+                RadioListTile(
+                    title: h2Text('Есть средняя вероятность задремать'),
+                    value: theatre,
+                    groupValue: 2,
+                    onChanged: ((value) => setState(() {
+                          theatre = 2;
+                        }))),
+                RadioListTile(
+                    title: h2Text('Есть высокая вероятность задремать'),
+                    value: theatre,
+                    groupValue: 3,
+                    onChanged: ((value) => setState(() {
+                          theatre = 3;
+                        }))),
+              ],
+            )),
+        Card(
+            elevation: 2,
+            child: Column(
+              children: [
+                ListTile(
+                  title: h1Text(
+                      'Вероятность задремать в качестве пассажира в машине при движении без остановки в течение часа'),
+                ),
+                RadioListTile(
+                    title: h2Text('Никогда бы не задремал'),
+                    value: car,
+                    groupValue: 0,
+                    onChanged: ((value) => setState(() {
+                          car = 0;
+                        }))),
+                RadioListTile(
+                    title: h2Text('Есть небольшой шанс задремать'),
+                    value: car,
+                    groupValue: 1,
+                    onChanged: ((value) => setState(() {
+                          car = 1;
+                        }))),
+                RadioListTile(
+                    title: h2Text('Есть средняя вероятность задремать'),
+                    value: car,
+                    groupValue: 2,
+                    onChanged: ((value) => setState(() {
+                          car = 2;
+                        }))),
+                RadioListTile(
+                    title: h2Text('Есть высокая вероятность задремать'),
+                    value: car,
+                    groupValue: 3,
+                    onChanged: ((value) => setState(() {
+                          car = 3;
+                        }))),
+              ],
+            )),
+        Card(
+            elevation: 2,
+            child: Column(
+              children: [
+                ListTile(
+                  title: h1Text(
+                      'Вероятность задремать во время отдыха в кровати во второй половине дня, если позволяет ситуация'),
+                ),
+                RadioListTile(
+                    title: h2Text('Никогда бы не задремал'),
+                    value: bed,
+                    groupValue: 0,
+                    onChanged: ((value) => setState(() {
+                          bed = 0;
+                        }))),
+                RadioListTile(
+                    title: h2Text('Есть небольшой шанс задремать'),
+                    value: bed,
+                    groupValue: 1,
+                    onChanged: ((value) => setState(() {
+                          bed = 1;
+                        }))),
+                RadioListTile(
+                    title: h2Text('Есть средняя вероятность задремать'),
+                    value: bed,
+                    groupValue: 2,
+                    onChanged: ((value) => setState(() {
+                          bed = 2;
+                        }))),
+                RadioListTile(
+                    title: h2Text('Есть высокая вероятность задремать'),
+                    value: bed,
+                    groupValue: 3,
+                    onChanged: ((value) => setState(() {
+                          bed = 3;
+                        }))),
+              ],
+            )),
+        Card(
+            elevation: 2,
+            child: Column(
+              children: [
+                ListTile(
+                  title: h1Text(
+                      'Вероятность задремать сидя в спокойной обстановке после обеда без потребления алкоголя'),
+                ),
+                RadioListTile(
+                    title: h2Text('Никогда бы не задремал'),
+                    value: alcohol,
+                    groupValue: 0,
+                    onChanged: ((value) => setState(() {
+                          alcohol = 0;
+                        }))),
+                RadioListTile(
+                    title: h2Text('Есть небольшой шанс задремать'),
+                    value: alcohol,
+                    groupValue: 1,
+                    onChanged: ((value) => setState(() {
+                          alcohol = 1;
+                        }))),
+                RadioListTile(
+                    title: h2Text('Есть средняя вероятность задремать'),
+                    value: alcohol,
+                    groupValue: 2,
+                    onChanged: ((value) => setState(() {
+                          alcohol = 2;
+                        }))),
+                RadioListTile(
+                    title: h2Text('Есть высокая вероятность задремать'),
+                    value: alcohol,
+                    groupValue: 3,
+                    onChanged: ((value) => setState(() {
+                          alcohol = 3;
+                        }))),
+              ],
+            )),
+        Card(
+            elevation: 2,
+            child: Column(
+              children: [
+                ListTile(
+                  title: h1Text(
+                      'Вероятность задремать сидя и разговаривая с кем-либо'),
+                ),
+                RadioListTile(
+                    title: h2Text('Никогда бы не задремал'),
+                    value: speak,
+                    groupValue: 0,
+                    onChanged: ((value) => setState(() {
+                          speak = 0;
+                        }))),
+                RadioListTile(
+                    title: h2Text('Есть небольшой шанс задремать'),
+                    value: speak,
+                    groupValue: 1,
+                    onChanged: ((value) => setState(() {
+                          speak = 1;
+                        }))),
+                RadioListTile(
+                    title: h2Text('Есть средняя вероятность задремать'),
+                    value: speak,
+                    groupValue: 2,
+                    onChanged: ((value) => setState(() {
+                          speak = 2;
+                        }))),
+                RadioListTile(
+                    title: h2Text('Есть высокая вероятность задремать'),
+                    value: speak,
+                    groupValue: 3,
+                    onChanged: ((value) => setState(() {
+                          speak = 3;
+                        }))),
+              ],
+            )),
+        Card(
+            elevation: 2,
+            child: Column(
+              children: [
+                ListTile(
+                  title: h1Text(
+                      'Вероятность задремать за рулём машины, остановившись на несколько минут на светофоре или в пробке'),
+                ),
+                RadioListTile(
+                    title: h2Text('Никогда бы не задремал'),
+                    value: drive,
+                    groupValue: 0,
+                    onChanged: ((value) => setState(() {
+                          drive = 0;
+                        }))),
+                RadioListTile(
+                    title: h2Text('Есть небольшой шанс задремать'),
+                    value: drive,
+                    groupValue: 1,
+                    onChanged: ((value) => setState(() {
+                          drive = 1;
+                        }))),
+                RadioListTile(
+                    title: h2Text('Есть средняя вероятность задремать'),
+                    value: drive,
+                    groupValue: 2,
+                    onChanged: ((value) => setState(() {
+                          drive = 2;
+                        }))),
+                RadioListTile(
+                    title: h2Text('Есть высокая вероятность задремать'),
+                    value: drive,
+                    groupValue: 3,
+                    onChanged: ((value) => setState(() {
+                          drive = 3;
+                        }))),
+              ],
+            )),
+        epworthResult(read, tv, theatre, car, bed, alcohol, speak, drive)
       ],
     );
   }
+}
+
+Widget epworthResult(int read, int tv, int theatre, int car, int bed,
+    int alcohol, int speak, int drive) {
+  int result = read + tv + theatre + car + bed + alcohol + speak + drive;
+  String textResult = '';
+
+  if (result <= 2) {
+    textResult = 'Норма';
+  }
+  if (result > 2 && result <= 8) {
+    textResult = 'Инсомния (бессонница)';
+  }
+  if (result > 8 && result <= 15) {
+    textResult = 'Синдром обструктивного апноэ сна';
+  }
+  if (result > 16) {
+    textResult = 'Нарколепсия (гиперсомния центрального генеза)';
+  }
+
+  return Card(
+    elevation: 2,
+    color: Colors.amber,
+    child: ListTile(
+      title: h2Text(textResult),
+    ),
+  );
 }
---
+++ windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,24 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "C:\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "C:\\Users\\Vlad\\Desktop\\neurology_flutter" PROJECT_DIR)
+
+set(FLUTTER_VERSION "1.0.0+1" PARENT_SCOPE)
+set(FLUTTER_VERSION_MAJOR 1 PARENT_SCOPE)
+set(FLUTTER_VERSION_MINOR 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_PATCH 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_BUILD 1 PARENT_SCOPE)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=C:\\flutter"
+  "PROJECT_DIR=C:\\Users\\Vlad\\Desktop\\neurology_flutter"
+  "FLUTTER_ROOT=C:\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=C:\\Users\\Vlad\\Desktop\\neurology_flutter\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=C:\\Users\\Vlad\\Desktop\\neurology_flutter"
+  "FLUTTER_TARGET=C:\\Users\\Vlad\\Desktop\\neurology_flutter\\lib\\main.dart"
+  "DART_DEFINES=RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ=="
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=C:\\Users\\Vlad\\Desktop\\neurology_flutter\\.dart_tool\\package_config.json"
+)
